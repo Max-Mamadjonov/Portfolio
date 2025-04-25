d35ffbedee9a43b4d549d88ab99c94e0dd4af14e
Max-Mamadjonov <magamed.maga.box@gmail.com>
2025-04-25 18:10:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+// //   var a = prompt('Hello! , what is your name bro ?');
+// //   x=document.getElementById('span');
+// //   b = a.trim(); 
+// //   c = a.split(' ');
+// // x.innerHTML=a;
+
+
+
+// function seeH() {
+//     var a = prompt('Do you want to see all my HTML works?');
+//     b = a.trim();
+//     // c = a.split(' ');
+//     if (a == 'Yes') {
+//         alert(confirm('OK! Click on the OK.'));
+//     }
+//     else{
+//        alert('Goodbye!') ;
+//     }
+// }
+
+// function seeC() {
+//     var a = prompt('Do you want to see all my CSS works?');
+//     b = a.trim();
+//     // c = a.split(' ');
+//     if (a == 'Yes') {
+//         alert(confirm('OK! Click on the OK.'));
+//     }
+//     else{
+//        alert('Goodbye!') ;
+//     }
+// }
+
+// function seeJ() {
+//     var a = prompt('Do you want to see all my JavaScript works?');
+//     b = a.trim();
+//     // c = a.split(' ');
+//     if (a == 'Yes') {
+//         alert(confirm('OK! Click on the OK.'));
+//     }
+//     else{
+//        alert('Goodbye!') ;
+//     }
+// }
+
+
+// function tel() {
+//     var a = prompt('Want  to discuss with me?');
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
